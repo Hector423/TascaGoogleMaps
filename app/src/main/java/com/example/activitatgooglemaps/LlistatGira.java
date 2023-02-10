7bb78ffe0153eb1f7e0823d760a2df0682cfe21a
Héctor Vallvé <hectorvallveg@gmail.com>
2023-02-10 16:55:35 +0100

    pantalla llistat

---
+++ app/src/main/java/com/example/activitatgooglemaps/LlistatGira.java
@@ -0,0 +1,14 @@
+package com.example.activitatgooglemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LlistatGira extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llistat_gira);
+    }
+}
