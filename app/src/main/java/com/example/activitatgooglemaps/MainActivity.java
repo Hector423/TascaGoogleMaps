4cff35834d76675c0140aee552928072907c8f90
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-02-23 17:46:03 +0100

    Two buttons in MainActivity.java

--- app/src/main/java/com/example/activitatgooglemaps/MainActivity.java
+++ app/src/main/java/com/example/activitatgooglemaps/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.activitatgooglemaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,22 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMapa = findViewById(R.id.btnMapa);
+        Button btnLlista = findViewById(R.id.btnLlista);
+        btnMapa.setOnClickListener(v->ObrirMapa());
+        btnLlista.setOnClickListener(v->ObrirLlista());
+    }
+
+    public void ObrirMapa()
+    {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void ObrirLlista()
+    {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
     }
 }
