19016fb60b3df21e9eb1ed3874b7cb993972a26b
Héctor Vallvé <hectorvallveg@gmail.com>
2023-02-24 15:50:27 +0100

    funciona scrollview

--- app/src/main/java/com/example/activitatgooglemaps/LlistatGira.java
+++ app/src/main/java/com/example/activitatgooglemaps/LlistatGira.java
@@ -14,29 +14,29 @@ public class LlistatGira extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_llistat_gira);
+        setContentView(R.layout.layout_llistat);
 
-    spinner = findViewById(R.id.spinner);
-    spinner2 = findViewById(R.id.spinner2);
-
-
-        String dropdownOptions[] = new String[3];
-
-        dropdownOptions[0] = "asd";
-        dropdownOptions[1] = "asd";
-        dropdownOptions[2] = "asd";
+//    spinner = findViewById(R.id.spinner);
+//    spinner2 = findViewById(R.id.spinner2);
 
 
-
-
-
-        ArrayAdapter<String> adapterView1 = new ArrayAdapter<String>
-                (
-                        this,
-                        android.R.layout.simple_spinner_item, dropdownOptions
-                );
-        adapterView1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapterView1);
+//        String dropdownOptions[] = new String[3];
+//
+//        dropdownOptions[0] = "asd";
+//        dropdownOptions[1] = "asd";
+//        dropdownOptions[2] = "asd";
+//
+//
+//
+//
+//
+//        ArrayAdapter<String> adapterView1 = new ArrayAdapter<String>
+//                (
+//                        this,
+//                        android.R.layout.simple_spinner_item, dropdownOptions
+//                );
+//        adapterView1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapterView1);
 
 
 
