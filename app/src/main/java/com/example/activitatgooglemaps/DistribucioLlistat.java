413c74952a6accdb9cbfa667102aa94269829fc3
Héctor Vallvé <hectorvallveg@gmail.com>
2023-02-25 19:49:00 +0100

    copia de seguretat per si pasen coses jijjiji

---
+++ app/src/main/java/com/example/activitatgooglemaps/DistribucioLlistat.java
@@ -0,0 +1,80 @@
+package com.example.activitatgooglemaps;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DistribucioLlistat extends RecyclerView.Adapter<Adaptador> {
+
+    List<String> paisos;
+    List<String> localitat;
+    List<String> escenari;
+
+
+    public DistribucioLlistat(List<String> paisos, List<String> localitat, List<String> escenari){
+        this.paisos = paisos;
+    }
+
+    @NonNull
+    @Override
+    public Adaptador onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_distribucio_llistat, parent, false);
+        return new Adaptador(view).adaptador(this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adaptador holder, int position) {
+        holder.textPais.setText(paisos.get(position));
+        holder.textLocalitat.setText(paisos.get(position));
+        holder.textEstadi.setText(paisos.get(position));
+        holder.textViewData.setText(paisos.get(position));
+        holder.textViewComprar.setText(paisos.get(position));
+        holder.textViewBuscar.setText(paisos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return paisos.size();
+    }
+}
+
+class Adaptador extends RecyclerView.ViewHolder {
+
+    TextView textPais, textLocalitat, textEstadi, textViewData, textViewComprar, textViewBuscar;
+    ImageView imageViewLogo;
+    ImageButton botoLinkEntrades, botoMapaLloc;
+    private DistribucioLlistat distribucioLlistat;
+
+    public Adaptador(@NonNull View itemView) {
+        super(itemView);
+
+        textPais = itemView.findViewById(R.id.textViewPais);
+        textLocalitat = itemView.findViewById(R.id.textViewLocalitat);
+        textEstadi = itemView.findViewById(R.id.textViewNomEstadi);
+        textViewData = itemView.findViewById(R.id.textViewData);
+        textViewComprar = itemView.findViewById(R.id.textViewComprar);
+        textViewBuscar = itemView.findViewById(R.id.textViewBuscar);
+        imageViewLogo = itemView.findViewById(R.id.imageViewLogo);
+//        botoLinkEntrades.findViewById(R.id.botoLinkEntrades).setOnClickListener(view -> {
+//
+//        });
+//        botoMapaLloc.findViewById(R.id.botoMapaLloc).setOnClickListener(view -> {
+//
+//        });
+    }
+    public Adaptador adaptador(DistribucioLlistat distribucioLlistat){
+        this.distribucioLlistat = distribucioLlistat;
+        return this;
+    }
+    
+}
--- app/src/main/java/com/example/activitatgooglemaps/LlistatGira.java
+++ app/src/main/java/com/example/activitatgooglemaps/LlistatGira.java
@@ -1,15 +1,14 @@
 package com.example.activitatgooglemaps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Spinner;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -17,9 +16,8 @@ import org.xml.sax.SAXException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -28,51 +26,54 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class LlistatGira extends AppCompatActivity {
 
-    private ListView listView;
-    private HashMap<String, String> llistatXML = new HashMap<>(10);
+    private HashMap<String, String> llistatXML = new HashMap<>();
+    private RecyclerView recyclerView;
     private ArrayList<String> arrayList = new ArrayList<>();
     private ImageView imageView;
     private ArrayAdapter<String> lAdapter;
+    private List<String> paisos = new LinkedList<>();
+    private List<String> localitat = new LinkedList<>();
+    private List<String> escenari = new LinkedList<>();
+    private List<String> data = new LinkedList<>();
+    private List<String> link = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_llistat);
+        List<String> items = new LinkedList<>();
 
-        listView = findViewById(R.id.listadoConcierto);
-        imageView = findViewById(R.id.imagenListado);
         try {
             InputStream input = getAssets().open("concerts.xml");
             DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = builderFactory.newDocumentBuilder();
             Document doc = docBuilder.parse(input);
             NodeList nList = doc.getElementsByTagName("concert");
-
+            
             for(int i = 0; i< nList.getLength(); i++){
                 Node node = nList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE){
-                    llistatXML.put("Apais", node.getChildNodes().item(1).getTextContent());
-                    llistatXML.put("Blocalitat", node.getChildNodes().item(3).getTextContent());
-                    llistatXML.put("Cescenari", node.getChildNodes().item(5).getTextContent());
-                    llistatXML.put("DlinkEntrades", node.getChildNodes().item(7).getTextContent());
-                    arrayList.addAll(llistatXML.values());
 
+                    paisos.add(node.getChildNodes().item(1).getTextContent());
+                    localitat.add(node.getChildNodes().item(3).getTextContent());
+                    escenari.add(node.getChildNodes().item(5).getTextContent());
+
+                    recyclerView = findViewById(R.id.listadoConcierto);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                    DistribucioLlistat distribucioLlistat = new DistribucioLlistat(paisos, localitat, escenari);
+                    recyclerView.setAdapter(distribucioLlistat);
+                    distribucioLlistat.notifyItemInserted(1);
                 }
             }
-            lAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayList);
-            listView.setAdapter(lAdapter);
-        }catch (IOException e) {
+        }catch (
+                IOException e) {
             e.printStackTrace();
-        } catch (ParserConfigurationException e) {
+        } catch (
+                ParserConfigurationException e) {
             e.printStackTrace();
-        } catch (SAXException e) {
+        } catch (
+                SAXException e) {
             e.printStackTrace();
         }
-
-
     }
-
-
-
-
 }
